891948c03381d0d9d912e1f2730b258359063eaf
Anshul Mittal <mittalanshul091@gmail.com>
2023-12-08 20:30:54 +0530

    added readme

--- bots/bot.js
+++ bots/bot.js
@@ -11,3 +11,16 @@ const { time } = require('console');
 const sqlite3 = require('sqlite3').verbose();
 
 
+require('dotenv').config();
+const { Client, List, Buttons, Contact, LocalAuth } = require('whatsapp-web.js');
+const qrcode = require('qrcode-terminal');
+const express = require('express');
+const fs = require('fs');
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+const { get } = require('request');
+const { time } = require('console');
+const sqlite3 = require('sqlite3').verbose();
+
+
