6a12a6cf790abedede8e02081c1be632a7f7b5da
Anshul Mittal <mittalanshul091@gmail.com>
2023-12-09 09:23:42 +0530

    basic code to check single client status

--- bots/bot.js
+++ bots/bot.js
@@ -24,3 +24,17 @@ const { time } = require('console');
 const sqlite3 = require('sqlite3').verbose();
 
 
+var db = new sqlite3.Database('./waas/db.sqlite3', (err) => {
+    if (err) {
+        console.error(err.message);
+    }
+    console.log('Connected to the my database.');
+});
+
+
+const PORT = process.env.HOST_PORT;
+const DJANGO_URL = process.env.DJANGO_URL;
+console.log("DJANGO_URL: " + DJANGO_URL);
+console.log("PORT: " + PORT);
+
+// helper function to save the new message to the database
