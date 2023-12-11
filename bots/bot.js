e96e55ff4cab5ece05e8c0040b21952d0bd1960c
Anshul Mittal <mittalanshul091@gmail.com>
2023-12-12 02:17:35 +0530

    create website api

--- bots/bot.js
+++ bots/bot.js
@@ -79,3 +79,16 @@ async function getLatestMessagesAfterTimestamp(client, chatid, time) {
 // Convert unix to DateTimeField
 
 function unix2DateTime(unixTimestamp) {
+    const date = new Date(unixTimestamp);
+
+// Extract date components
+    const year = date.getFullYear();
+    const month = (date.getMonth() + 1).toString().padStart(2, '0'); // Months are zero-indexed, so January is 0
+    const day = date.getDate().toString().padStart(2, '0');
+    const hours = date.getHours().toString().padStart(2, '0');
+    const minutes = date.getMinutes().toString().padStart(2, '0');
+    const seconds = date.getSeconds().toString().padStart(2, '0');
+    const milliseconds = date.getMilliseconds().toString().padStart(3, '0');
+
+    // Construct the datetimeField string in the format expected
+    let datetimeField = `${year}-${month}-${day}T${hours}:${minutes}:${seconds}.${milliseconds}`;
