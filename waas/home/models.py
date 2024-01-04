621c94ebe0bc77a745241c87fd845ae1fa18e402
Anshul Mittal <mittalanshul091@gmail.com>
2024-01-04 21:28:05 +0530

    Downloadable json file

--- waas/home/models.py
+++ waas/home/models.py
@@ -4,3 +4,10 @@ import uuid
 # Create your models here.
 
 BOT_TYPES = (
+    ('semi-active', 'Semi-Active'),
+    ('active', 'Active'),
+)
+
+class Bot(models.Model):
+    id = models.CharField(max_length=32, primary_key=True, default=uuid.uuid4, editable=False)
+    type = models.CharField(max_length=32, default='semi-active', choices=BOT_TYPES)
