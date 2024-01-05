299e87b291f5752678b5c19f82de64759d5df218
Anshul Mittal <mittalanshul091@gmail.com>
2024-01-05 17:16:39 +0530

    created static and base template

--- waas/home/models.py
+++ waas/home/models.py
@@ -11,3 +11,10 @@ BOT_TYPES = (
 class Bot(models.Model):
     id = models.CharField(max_length=32, primary_key=True, default=uuid.uuid4, editable=False)
     type = models.CharField(max_length=32, default='semi-active', choices=BOT_TYPES)
+    botStarted = models.IntegerField(default=0)
+    name = models.CharField(max_length=100)    # name of the bot
+    username = models.CharField(max_length=100, blank=False, null=False)
+    mobile = models.CharField(max_length=16)
+
+    def __str__(self):
+        return f'{self.id} | {self.name} | {self.username} | {self.type}'
